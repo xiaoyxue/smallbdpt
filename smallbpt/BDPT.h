334b51dde8ec5771fe1a62f68eb48b736a4a802c
FAREAST\xiaoyxue <xiaoyxue@microsoft.com>
2022-07-31 13:25:19 +0800

    no message

--- smallbpt/BDPT.h
+++ smallbpt/BDPT.h
@@ -4,7 +4,7 @@
 #include "Integrator.h"
 #include "Camera.h"
 #include "Sampler.h"
-#include "Intersect.h"
+#include "Intersection.h"
 #include <vector>
 #include "Geometry.h"
 #include "PathVertex.h"
@@ -39,42 +39,40 @@ private:
 class BidirectionalPathTracing : public Integrator {
 public:
 	BidirectionalPathTracing() {}
-	BidirectionalPathTracing(Sampler *smp, Camera *cam, Film *_film, int maxdepth, int samplePerPixel,
-		bool _visualizeStrategies, bool _visualizeWeight) :
-		sampler(smp), camera(cam), film(_film), maxDepth(maxdepth), spp(samplePerPixel),
-		visualizeStrategies(_visualizeStrategies), visualizeWeight(_visualizeWeight){}
-	void Render();
+	BidirectionalPathTracing(Sampler *pSampler,int maxDepth, int spp, bool visualizeStrategies, bool visualizeWeight) :
+		mpSampler(pSampler), mMaxDepth(maxDepth), mSpp(spp),
+		mVisualizeStrategies(visualizeStrategies), mVisualizeWeight(visualizeWeight){}
+	void Render(const Scene &scene, const Camera& camera);
 private:
-	bool visualizeStrategies;
-	bool visualizeWeight;
+	bool mVisualizeStrategies;
+	bool mVisualizeWeight;
 
-	int spp, maxDepth;
-	Film *film;
-	Camera *camera;
-	Sampler *sampler;
+	int mSpp, mMaxDepth;
+	Sampler *mpSampler;
 	//std::vector<PathVertex> LightPath;
 };
 
-int GenerateLightPath(Sampler &sampler, std::vector<PathVertex> &LightPath, int maxdepth);
-int GenerateCameraPath(Sampler &sampler, std::vector<PathVertex> &CameraPath, const Camera &camera, 
-	const Ray &cameraRay, int maxdepth);
-int Trace(const Ray &ray, Vec3 Throughput, double PdfFwd, Sampler &sampler, 
+int GenerateLightPath(const Scene& scene, Sampler &sampler, std::vector<PathVertex> &LightPath, int maxdepth);
+
+int GenerateCameraPath(const Scene& scene, const Camera& camera, Sampler &sampler, std::vector<PathVertex> &CameraPath, const Ray &cameraRay, int maxdepth);
+
+int Trace(const Scene& scene, const Ray &ray, Vec3 Throughput, double PdfFwd, Sampler &sampler,
 	std::vector<PathVertex> &Path, int depth, int maxDepth);
-Vec3 ConnectBDPT(std::vector<PathVertex> &LightPath, std::vector<PathVertex> &CameraPath,
-	int s, int t, Camera &camera, Sampler &sampler, Vec3 *pRaster, bool *inScreen, double *MISRecord);
+
+Vec3 ConnectBDPT(const Scene& scene, const Camera& camera, Sampler& sampler, 
+	std::vector<PathVertex>& LightPath, std::vector<PathVertex>& CameraPath, int s, int t, Vec3* pRaster, bool* inScreen, double* MISRecord);
+
 double ConvertSolidToArea(double PdfW, const PathVertex &Vertex, const PathVertex &nxt);
+
 double G(const PathVertex &VertexA, const PathVertex &VertexB);
-bool IsConnectible(const Vec3 &PointA, const Vec3 &PointB);
+
+bool IsConnectable(const Scene& scene, const Vec3 &PointA, const Vec3 &PointB);
+
 double MISWeight(std::vector<PathVertex> &LightPath, std::vector<PathVertex> &CameraPath,
 	int s, int t, PathVertex &sampled);
-double MISWeight2(const std::vector<PathVertex> &LightPath, const std::vector<PathVertex> &CameraPath,
-	int s, int t);
-double MISWeight3(std::vector<PathVertex> &LightPath, std::vector<PathVertex> &CameraPath,
+
+double MISWeight2(std::vector<PathVertex> &LightPath, std::vector<PathVertex> &CameraPath,
 	int s, int t, PathVertex &sampled);
-double PathPdf(const std::vector<PathVertex> &LightPath, const std::vector<PathVertex> &CameraPath,
-	int s, int t, bool specialPath = false);
-double MISWeight4(const std::vector<PathVertex> &LightPath, const std::vector<PathVertex> &CameraPath,
-	int s, int t);
 
 double Path_Pdf(const std::vector<PathVertex> &Path, int s, int t);
 #endif
